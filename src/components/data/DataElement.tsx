c43fc0a2910a74fd4c8015e70c327c91a5e3a6be
nickblock <nickblock@hotmail.com>
2024-09-21 17:07:52 +0100

    Table file

--- src/components/data/DataElement.tsx
+++ src/components/data/DataElement.tsx
@@ -1,35 +1,6 @@
-import { FC, useEffect, useState } from 'react';
-import {
-  SortingState,
-  createColumnHelper,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-
-const columnHelper = createColumnHelper<DataAttribute>();
-
-const columns = [
-  columnHelper.accessor("name", {
-    header: () => "Name",
-    cell: (info) => info.getValue(),
-  }),
-  columnHelper.accessor("value", {
-    header: () => "Value",
-    cell: (info) => info.getValue(),
-  }),
-];
-
-interface DataAttribute {
-  name: string,
-  value: number,
-  unit: string
-}
-
-export interface DataElement {
-  title: string,
-  attributes: DataAttribute[]
-}
+import { FC } from 'react';
+import Table, { Props } from "./Table"
+import { DataElement } from './DataDef';
 
 export type DataElementDisplayProps = {
   data: DataElement
@@ -37,58 +8,14 @@ export type DataElementDisplayProps = {
 
 const DataElementDisplay: FC<DataElementDisplayProps> = (props: DataElementDisplayProps) => {
 
-  const attributeData = props.data.attributes
-
-  const table = useReactTable({
-    data: props.data.attributes,
-    columns,
-    debugTable: true,
-    getCoreRowModel: getCoreRowModel()
-  });
-
+  const tableProps = {
+    data: props.data.attributes
+  }
   return (
     <div>
       <h1>{props.data.title}</h1>
-      <table className="data-table">
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id} className="data-table-cell">
-                  <div
-                    {...{
-                      className: header.column.getCanSort()
-                        ? "cursor-pointer select-none"
-                        : "",
-                      onClick: header.column.getToggleSortingHandler(),
-                    }}
-                  >
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                    {{
-                      asc: " 🔼",
-                      desc: " 🔽",
-                    }[header.column.getIsSorted() as string] ?? null}
-                  </div>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id} className="data-table-cell">
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+
+      <Table {...tableProps} />
 
     </div>
   )
---
+++ src/components/data/Table.tsx
@@ -0,0 +1,83 @@
+import { FC } from 'react';
+import { DataAttribute } from "./DataDef"
+
+
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+const columnHelper = createColumnHelper<DataAttribute>();
+
+const columns = [
+  columnHelper.accessor("name", {
+    header: () => "Name",
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor("value", {
+    header: () => "Value",
+    cell: (info) => info.getValue(),
+  }),
+];
+
+export type Props = {
+  data: DataAttribute[]
+}
+
+const Table: FC<Props> = ({ data }: Props) => {
+
+  const table = useReactTable({
+    data: data,
+    columns,
+    debugTable: true,
+    getCoreRowModel: getCoreRowModel()
+  });
+
+  return (
+    <table className="data-table">
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id} className="data-table-cell">
+                <div
+                  {...{
+                    className: header.column.getCanSort()
+                      ? "cursor-pointer select-none"
+                      : "",
+                    onClick: header.column.getToggleSortingHandler(),
+                  }}
+                >
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                  {{
+                    asc: " 🔼",
+                    desc: " 🔽",
+                  }[header.column.getIsSorted() as string] ?? null}
+                </div>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id} className="data-table-cell">
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+
+  )
+}
+
+export default Table
