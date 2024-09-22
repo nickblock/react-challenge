2b18feb7b2b80261e11927024956e223540cea89
nickblock <nickblock@hotmail.com>
2024-09-22 07:29:24 +0100

    login box functionality

--- src/components/login/LoginBox.tsx
+++ src/components/login/LoginBox.tsx
@@ -4,34 +4,55 @@ type LoginFormProps = {
   submitState: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+type InputBoxProps = {
+  label: string,
+  prompt: string
+  setSubmitEnabled: React.Dispatch<React.SetStateAction<boolean>>
+}
+const InputBox: FC<InputBoxProps> = (props: InputBoxProps) => {
+  const [value, setValue] = useState(props.prompt)
+  const [focused, setFocused] = useState(false)
+
+  function onFocus() {
+    if (!focused) {
+      setFocused(true)
+      setValue('')
+    }
+  }
+  return (
+    <label>
+      {props.label}:
+      <input type="text" value={value} onFocus={onFocus} onChange={e => {
+        setValue(e.target.value); props.setSubmitEnabled(true)
+      }} />
+    </label>
+  )
+}
+
 const LoginForm: FC<LoginFormProps> = ({ submitState }: LoginFormProps) => {
 
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [submitEnabled, setSubmitEnabled] = useState(false)
 
   function handleLogin(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    // Code to handle login goes here
+
     submitState(true);
   }
 
   return (
-    <div className="popup">
+    <div className="login-form">
+      <div className="login-title">
+        <h1> Dashboard </h1>
+        Welcome to the dashboard. Enter a valid username and password to proceed.
+      </div>
       <div className="popup-inner">
-        <h2>Login</h2>
         <form onSubmit={handleLogin}>
-          <label>
-            Username:
-            <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-          </label>
-          <label>
-            Password:
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-          </label>
-          <button type="submit">Login</button>
+          <InputBox label='Username' prompt='usen@pass.com' setSubmitEnabled={setSubmitEnabled} />
+          <InputBox label='Password' prompt='password' setSubmitEnabled={setSubmitEnabled} />
+          <button type="submit" disabled={!submitEnabled} >Login</button>
         </form>
       </div>
-    </div>
+    </div >
   )
 
 }
